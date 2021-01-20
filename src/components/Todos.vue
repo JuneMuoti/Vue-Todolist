12ced358109e47f6554fe9c1d295ffe67ab3890c
Geoffrey Nzomo <GNzomo@hq.horizoncontactcenters.com>
2021-01-20 16:13:27 +0300

    Added my todo project files

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div v-for="todo in todos" v-bind:key="todo.id">
+            <Todoitem v-bind:todo="todo" v-on:del-todo="$emit('del-todo',todo.id)" />
+        </div>
+    </div>
+</template>
+<script>
+import Todoitem from './Todoitem.vue';
+export default {
+    name:"Todos",
+    components:{
+        Todoitem
+    },
+    props:["todos"]
+}
+</script>
+
+
+<style scoped>
+h1{
+    display: inline;
+   color: aqua;
+   margin-left: 50px;
+}
+</style>
